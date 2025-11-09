cff66e6a785f861285477541dc5bfb92bbf890cd
awatech <awatech12@gmail.com>
2025-11-09 06:20:58 -0800

    register models in admin

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from social.models import Profile, Post, PostImage, PostComment, Message, Notification, Channel, ChannelMessage
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(PostImage)
+admin.site.register(PostComment)
+admin.site.register(Message)
+admin.site.register(Notification)
+admin.site.register(Channel)
+admin.site.register(ChannelMessage)
