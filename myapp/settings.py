2aa04ec9540ef72e445413b761670c13f89b9cd5
Ayobami Awatech <107240994+Awatech12@users.noreply.github.com>
2025-11-11 19:05:50 +0100

    Update DATABASES setting to use dj_database_url
    Refactor DATABASES setting to use dj_database_url for configuration.

--- myapp/settings.py
+++ myapp/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-from pathlib import Path
 import os
+import dj_database_url
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
@@ -113,11 +113,12 @@ CHANNEL_LAYERS = {
 }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # fallback for local development
+        conn_max_age=600,
+        env='DATABASE_URL'
+    )
     }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
