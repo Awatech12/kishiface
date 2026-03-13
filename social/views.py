3f795281d589f7e72716cc93b202e252a53ef02a
Ayobami Awatech <107240994+Awatech12@users.noreply.github.com>
2026-03-13 07:51:14 +0100

    Update views.py

--- social/views.py
+++ social/views.py
@@ -1313,6 +1313,7 @@ def send_message(request, username):
         reply_data = None
         if reply_to:
             reply_data = {
+                'message_id': str(reply_to.id),
                 'sender': reply_to.sender.username,
                 'message': reply_to.conversation,
                 'file_type': reply_to.file_type
@@ -1324,14 +1325,14 @@ def send_message(request, username):
             room_group_name,
             {
                 'type': 'chat_message',
-                'message_id': message.id,
+                'message_id': str(message.id),
                 'sender': request.user.username,
                 'sender_avatar': sender_avatar,
                 'receiver': receiver.username,
                 'message': message_text,
                 'file_type': file_type,
                 'file_url': file_url,
-                'time': message.chat_time,
+                'time': str(message.chat_time),
                 'reply_to': reply_data,
             }
         )
@@ -2293,3 +2294,4 @@ def online_status_api(request, user_id):
         return JsonResponse({'is_online': profile.is_online})
     except Profile.DoesNotExist:
         return JsonResponse({'is_online': False})
+
