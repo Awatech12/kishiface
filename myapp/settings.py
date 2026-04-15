4241969ecfcc9761271451431866534aa3970c16
awatech <awatech12@gmail.com>
2026-04-15 15:07:38 +0100

    changes made

--- myapp/settings.py
+++ myapp/settings.py
@@ -104,7 +104,7 @@ TEMPLATES = [
                 'social.context_processors.user_notifications',
                 'social.context_processors.follow_notifications_context',
                 'social.context_processors.channel_unread_processor',
-            ],
+            ], 
         },
     },
 ]
@@ -135,6 +135,24 @@ DATABASES = {
         env='DATABASE_URL'
     )
 }
+if USE_CLOUDINARY:
+    DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),  
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=True
+    )
+    }
+else:
+    DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE"),  
+        conn_max_age=600,
+        env='DATABASE_URL'
+    )
+    }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
